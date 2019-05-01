ab8f3e2fb155799560b9774ecc5ae45d3ac9006b
cks6215 <cks6215@unist.ac.kr>
2019-05-02 07:07:54 +0900

    Update settings.py

--- petandpeople/petandpeople/settings.py
+++ petandpeople/petandpeople/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/cks9896/petd/petandpeople/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/cks9896/petd/petandpeople/media'
